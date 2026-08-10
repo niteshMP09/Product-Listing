adb7afb985ea4ca630633dd74e07d963a36e4d07
niteshMP09 <niteshsikarwar0307@gmail.com>
2026-08-10 18:21:44 +0530

    refactor: add hooks barrel export

--- src/components/ProductCard/ProductCardSkeleton.tsx
+++ src/components/ProductCard/ProductCardSkeleton.tsx
@@ -1,4 +1,4 @@
-import Skeleton from "../common/Skeleton";
+import Skeleton from "../common/Skeleton/Skeleton";
 
 function ProductCardSkeleton() {
   return (
---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useDebounce } from "./useDebounce";
+export { default as useProductFilters } from "./useProductFilters";
+export { useProducts } from "./useProducts";
+export { default as useProductSorting } from "./useProductSorting";
