c187ff21ba73c04de20904138ee8cd11fae4e85b
niteshMP09 <niteshsikarwar0307@gmail.com>
2026-08-10 12:04:16 +0530

    chore: establish initial project architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import ProductListingPage from "./pages/ProductListingPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-      </h1>
-    </>
-  );
+  return <ProductListingPage />;
 }
 
 export default App;
---
+++ src/pages/ProductListingPage.tsx
@@ -0,0 +1,10 @@
+function ProductListingPage() {
+  return (
+    <main>
+      <h1>Product Listing Page</h1>
+      <p>Searchspring ecommerce experience</p>
+    </main>
+  );
+}
+
+export default ProductListingPage;
